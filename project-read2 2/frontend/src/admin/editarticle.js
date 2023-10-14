e4f22cfd1863eb24bfd936b4c9e69cb62d05d4cc
mhontoey2000 <teimiku@gmail.com>
2023-10-14 21:07:17 +0700

    แก้เสียงหน้าแก้ไขบทความในแอดมิน+เปลี่ยน UI ตัวเล่นเสียง

--- project-read2 2/frontend/src/admin/editarticle.js
+++ project-read2 2/frontend/src/admin/editarticle.js
@@ -6,6 +6,7 @@ import Button from "react-bootstrap/Button";
 import { useParams } from "react-router-dom";
 import "../styles/editall.css";
 import { useHistory } from 'react-router-dom';
+import formatTime from '../formattime';
 
 function Editarticle() {
     const history = useHistory();
@@ -16,54 +17,80 @@ function Editarticle() {
     const [image, setImage] = useState("");
     const [selectedImageFile, setSelectedImageFile] = useState(null);
 
-    const [sounds, setSounds] = useState("");
-    const [vocabs, setVocabs] = useState("");
+    const [audioProgress, setAudioProgress] = useState(0);
+    const [audioUrl, setAudioUrl] = useState(null);
+    const [audioRef, setAudioRef] = useState(null);
     const [isPlaying, setIsPlaying] = useState(false);
-    const [audio] = useState(new Audio());
     const [duration, setDuration] = useState(0);
     const [currentTime, setCurrentTime] = useState(0);
-
-    const playAudio = () => {
-        if (audio.paused) {
-            audio.play();
-            setIsPlaying(true);
-        } else {
-            audio.pause();
-            setIsPlaying(false);
-        }
-    };
-
-    const updateAudioData = () => {
-        setDuration(audio.duration);
-        setCurrentTime(audio.currentTime);
-    };
-
-    audio.addEventListener("timeupdate", updateAudioData);
+    let selectedArticle = null; // Define the selectedArticle variable here
 
     const handleImageChange = (event) => {
         const selectedImage = event.target.files[0];
-        setSelectedImageFile(selectedImage); 
-        setImage(URL.createObjectURL(selectedImage)); 
+        setSelectedImageFile(selectedImage);
+        setImage(URL.createObjectURL(selectedImage));
     };
 
-    const handleSoundUpload = (event) => {
-        const soundFile = event.target.files[0];
-        setSounds(soundFile);
-        audio.src = URL.createObjectURL(soundFile);
-    }
+    const playAudio = async (audioData) => {
+        if (isPlaying) {
+            setIsPlaying(false);
+            return;
+        }
+    
+        const blob = new Blob([new Uint8Array(audioData.data)], { type: 'audio/mpeg' });
+        const blobUrl = URL.createObjectURL(blob);
+        setAudioUrl(blobUrl);
+    };
+
+    const handlePause = () => {
+        setIsPlaying(false);
+        audioRef.pause();
+    };
+
+    const handleSeek = (time) => {
+        audioRef.currentTime = time;
+        setCurrentTime(time);
+        setAudioProgress((time / duration) * 100);
+    };
+
+    const handlePlayPause = () => {
+        if (isPlaying) {
+            setIsPlaying(false);
+            audioRef.pause();
+        } else {
+            setIsPlaying(true);
+            audioRef.play();
+        }
+    };
+
+    const handleAudioRef = (ref) => {
+        setAudioRef(ref);
+        if (ref) {
+            setDuration(ref.duration);
+        }
+    };
+
+    useEffect(() => {
+        if (audioRef) {
+            if (isPlaying) {
+                audioRef.play();
+            } else {
+                audioRef.pause();
+            }
+        }
+    }, [isPlaying, audioRef]);
 
     useEffect(() => {
         axios.get(`http://localhost:5004/api/article`)
             .then((response) => {
                 for (let i = 0; i < response.data.length; i++) {
                     if (response.data[i].article_id === articleid) {
-                        setBookName(response.data[i].book_name);
-                        setArticleName(response.data[i].article_name);
-                        setArticleDetail(response.data[i].article_detail);
-                        setImage(response.data[i].article_imagedata);
-                        setSounds(response.data[i].article_sounddata);
-                        setVocabs(response.data[i].vocabs);
-                        audio.src = response.data[i].article_sounddata;
+                        selectedArticle = response.data[i];
+                        setBookName(selectedArticle.book_name);
+                        setArticleName(selectedArticle.article_name);
+                        setArticleDetail(selectedArticle.article_detail);
+                        setImage(selectedArticle.article_imagedata);
+                        playAudio(selectedArticle.article_sounddata);
                         break;
                     }
                 }
@@ -71,11 +98,6 @@ function Editarticle() {
             .catch((error) => {
                 console.error(error);
             });
-
-        return () => {
-            audio.removeEventListener("timeupdate", updateAudioData);
-            audio.pause();
-        };
     }, [articleid]);
 
     const cancelEditArticle = () => {
@@ -83,22 +105,7 @@ function Editarticle() {
     }
 
     const editArticle = () => {
-        const formData = new FormData();
-        formData.append('article_id', articleid);
-        formData.append('book_name', bookName);
-        formData.append('article_name', articleName);
-        formData.append('article_detail', articleDetail);
-        formData.append('image', selectedImageFile);
-        formData.append('sounds', sounds);
-        formData.append('vocabs', vocabs);
-
-        axios.post('http://localhost:5004/api/updatearticle', formData)
-            .then((response) => {
-                history.push('/Page/allarticleadmin');
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        // Add code for uploading audio files or remove unrelated parts
     };
 
     return (
@@ -155,30 +162,27 @@ function Editarticle() {
                                     accept="image/*"
                                     onChange={handleImageChange}
                                 />
-                                {image && (
-                                    <img
-                                        src={image}
-                                        alt="Uploaded Image"
-                                        style={{ maxWidth: '100%', maxHeight: '200px' }}
-                                    />
+                                <div className='d-flex justify-content-center align-items-center'>
+                                    {image && (
+                                        <img
+                                            src={image}
+                                            alt="Uploaded Image"
+                                            style={{ maxWidth: '100%', maxHeight: '200px' }}
+                                        />
+                                    )}
+                                </div>
+                            </div>
+                            <div className='d-flex justify-content-center align-items-center'>
+                                {audioUrl && (
+                                    <div>
+                                        <audio
+                                            src={audioUrl}
+                                            ref={handleAudioRef}
+                                            controls
+                                        />
+                                    </div>
                                 )}
                             </div>
-                            <div className="mb-3">
-                                <label htmlFor="sound">เสียงของเนื้อหา</label>
-                                <input
-                                    type="file"
-                                    className="form-control"
-                                    id="sound"
-                                    accept="audio/*"
-                                    onChange={handleSoundUpload}
-                                />
-                            </div>
-                            <div>
-                                <Button className="play-button" onClick={playAudio}>
-                                    {isPlaying ? 'Pause' : 'Play'}
-                                </Button>
-                            </div>
-
                             <div className="btn-containerr">
                                 <div className="btn-group me-2">
                                     <Button
